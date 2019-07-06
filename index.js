ce467fa6fe80519e33562aac919ac61dc56a7505
fedeghe <fedeghe@gmail.com>
2019-07-06 23:14:00 +0200

    reject set

--- index.js
+++ index.js
@@ -8,19 +8,28 @@ function obfuscator(obj, options) {
         msg,
         pluginName = path.basename(path.dirname(__filename));
     options = options || {};
-    obj.content = ob.obfuscate(obj.content, options).getObfuscatedCode();
+    
     return function (solve, reject) {
-        fs.writeFile(obj.name, obj.content, function (err) {
-            if (err == null) {
-                msg = 'plugin ' + pluginName.white() + ' wrote ' + obj.name +' (' + self.getSize(obj.name) + ')';
-            } else {
-                console.log('[ERROR] js-obfuscator says:');
-                console.dir(err);
-                self.stop();
-            }
-            solve(obj);
+        try {
+            obj.content = ob.obfuscate(obj.content, options).getObfuscatedCode();
+            fs.writeFile(obj.name, obj.content, function (err) {
+                if (err == null) {
+                    msg = 'plugin ' + pluginName.white() + ' wrote ' + obj.name +' (' + self.getSize(obj.name) + ')';
+                } else {
+                    self.doErr(err, obj, pluginName);
+                }
+                err
+                    ? reject(`Plugin ${pluginName} write error:\n${err}`)
+                    : solve(obj);
+                self.notifyAndUnlock(start, msg);
+            })
+        } catch(err) {
+            err
+                ? reject(`Plugin ${pluginName} minification error:\n${err}`)
+                : solve(obj);
             self.notifyAndUnlock(start, msg);
-        })
+        }
+        
     }
 }
 obfuscator.ext = ['js', 'coffee', 'ts'];
